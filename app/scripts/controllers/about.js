5426bfa9eb56cf07aa663d25e97a52bf69003f9c
nazli bozoglu <nzlbozoglu@gmail.com>
2014-10-19 17:42:30 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteTakingAppNoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the noteTakingAppNoApp
+ */
+angular.module('noteTakingAppNoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteTakingAppNoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noteTakingAppNoApp
+ */
+angular.module('noteTakingAppNoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
