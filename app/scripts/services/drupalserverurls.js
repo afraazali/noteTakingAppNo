7d58f94bc265c9ac0eea5837ed6702732c1d6f21
Afraaz Ali <afraaz@appnovation.com>
2014-10-19 21:37:38 -0400

    correct the rest url

--- app/scripts/services/drupalserverurls.js
+++ app/scripts/services/drupalserverurls.js
@@ -9,6 +9,6 @@
  */
 angular.module('noteTakingAppNoApp')
   .constant('DrupalServerUrls', {
-    'REST_LINK_URL': 'http://d8.dev/rest/type/node/page',
+    'REST_LINK_URL': 'http://drupal8.codingtimes.com/rest/type/node/page',
     'DOMAIN': 'http://drupal8.codingtimes.com'
   });
