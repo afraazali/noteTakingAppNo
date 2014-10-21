57f4a9af357f78640f3ee92161963aab4a780b0d
Afraaz Ali <afraaz@appnovation.com>
2014-10-21 11:18:20 -0400

    supply full domain when logging in user

--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -8,13 +8,13 @@
  * Factory in the noteTakingAppNoApp.
  */
 angular.module('noteTakingAppNoApp')
-  .factory('UserService', ['$q', '$http', '$cookieStore', function ($q, $http, $cookieStore) {
+  .factory('UserService', ['$q', '$http', '$cookieStore', 'DrupalServerUrls', function ($q, $http, $cookieStore, DrupalServerUrls) {
 
     // Public API here
     return {
       login: function(username, password) {
         var deferred = $q.defer();
-        $http.post('/ng-auth', {'username': username, 'password': password}, {headers: {'Content-type': 'application/hal+json'}}).success(function(data) {
+        $http.post(DrupalServerUrls.DOMAIN + '/ng-auth', {'username': username, 'password': password}, {headers: {'Content-type': 'application/hal+json'}}).success(function(data) {
           if (typeof data.authed !== 'undefined') {
             $cookieStore.put('authenticated', data.authed);
             deferred.resolve();
